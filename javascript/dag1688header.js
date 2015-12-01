6ac6ae37e3aad836cfa79106b7e9b67d0784d5ba
jonnypeng <jonny.peng@qq.com>
2015-12-01 17:06:36 +0800

    上传网站的JS

---
+++ javascript/dag1688header.js
@@ -0,0 +1,6 @@
+<!--[if lte IE 9]>
+            dbimg.src = "http://dag1688.aliapp.com/wp-content/uploads/2015/11/brandleft.jpg";
+            dbimg.style.position = "relative";
+            dbimg.style.bottom = "110px";
+<![endif]-->
+
