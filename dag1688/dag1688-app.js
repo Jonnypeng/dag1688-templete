86abb12c8df78c4ed238c76681eedd7893e2e264
jonnypeng <jonny.peng@qq.com>
2016-03-25 13:22:17 +0800

    曾将APP的JS与CSS

---
+++ dag1688/dag1688-app.js
@@ -0,0 +1,102 @@
+var dagIframe = document.getElementById("iframe-dag");
+var navMenu = document.getElementById("nav-menu");
+var navMenuLi = navMenu.getElementsByTagName("li");
+var exitBtn = document.getElementById("exitBtn");
+var exit = document.getElementById("exit-model");
+var okExit = document.getElementById("okExit");
+var noExit = document.getElementById("noExit");
+var moreBtn = document.getElementById("more");
+var navFooter = document.getElementById("nav-footer");
+var closedBtn = document.getElementById("closed");
+var navFooterDiv = navFooter.getElementsByTagName("div");
+var menuOnOff = false;
+var moreOnOff = false;
+
+
+onload();
+
+closedBtn.onclick = function (){
+	navMenu.style.display = "none"	;
+	moreOnOff = false;
+};
+function onload(){
+	document.addEventListener("deviceready",onDeviceReady,false);
+		function onDeviceReady(){
+			document.addEventListener("menubutton",onMenuButton,false);
+		};
+
+		function onMenuButton(){
+		disable(menuOnOff);
+		};
+};
+		exitBtn.onclick = function(){
+			disable(menuOnOff);
+				exit.style.display = "block";
+};
+		
+	okExit.onclick = function (){
+		navigator.app.exitApp();
+};
+
+	noExit.onclick = function (){
+		exit.style.display = "none";
+};
+
+//========================底部菜单键的动作
+	moreBtn.onclick = function (){
+		disable(menuOnOff);
+	};
+
+
+
+//底部菜单
+for(var i = 0;i<navFooterDiv.length;i++){
+	navFooterDiv[0].onclick = function(){
+		frameSrc("http://www.dag1688.com/products_list/&Column_navigation01-002FCId=46&pageNo_Product_list01-105=1&pageSize_Product_list01-105=15.html");
+				};
+	navFooterDiv[1].onclick = function(){
+		frameSrc("http://dag1688.aliapp.com/search-product.html");
+				};
+	navFooterDiv[2].onclick = function(){
+		frameSrc("http://www.dag1688.com/products_brand/&Column_navigation01-001FCId=181&pageNo_ProductBrand_list01-001=1&pageSize_ProductBrand_list01-001=25.html#category-index");
+				};
+};
+
+
+//左侧菜单
+for(var i = 0;i<navMenuLi.length;i++){
+navMenuLi[0].onclick = function(){
+		frameSrc("http://www.dag1688.com/products_list/&Column_navigation01-002FCId=46&pageNo_Product_list01-105=1&pageSize_Product_list01-105=15.html");
+		disable(menuOnOff);
+};
+navMenuLi[1].onclick = function(){
+		frameSrc("http://www.dag1688.com/products_brand/&Column_navigation01-001FCId=181&pageNo_ProductBrand_list01-001=1&pageSize_ProductBrand_list01-001=25.html");
+		disable(menuOnOff);
+navMenuLi[2].onclick = function(){
+		frameSrc("http://www.dag1688.com/zhuanti/dagmap/wechat-map.html");
+		disable(menuOnOff);
+};
+navMenuLi[3].onclick = function(){
+		frameSrc("http://www.dag1688.com/zhuanti/dagmap/streetview.html");
+		disable(menuOnOff);
+};
+navMenuLi[4].onclick = function(){
+		frameSrc("http://dag1688.aliapp.com/");
+		disable(menuOnOff);
+};
+};
+
+function frameSrc(url){
+dagIframe.setAttribute("src",url);
+};
+
+	function disable(menuOnOff){
+		if(!menuOnOff){
+				navMenu.style.display = "block";
+				menuOnOff = true;
+}else if(menuOnOff){
+				navMenu.style.display = "none";
+				menuOnOff = false;
+};
+};
+
