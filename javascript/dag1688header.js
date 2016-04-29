c1ba3761c8df431cc3c4c70cffabd62f908a2bde
jonnypeng <jonny.peng@qq.com>
2016-04-29 10:34:56 +0800

    清理文件夹

--- javascript/dag1688header.js
+++
@@ -1,6 +0,0 @@
-<!--[if lte IE 9]>
-            dbimg.src = "http://dag1688.aliapp.com/wp-content/uploads/2015/11/brandleft.jpg";
-            dbimg.style.position = "relative";
-            dbimg.style.bottom = "110px";
-<![endif]-->
-
