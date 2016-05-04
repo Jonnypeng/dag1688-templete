4d11f81e498b18b702df4949bdead3cbe2dd4ea5
jonnypeng <jonny.peng@qq.com>
2016-05-04 17:15:37 +0800

    增加威达与麦太保的头部

---
+++ html_edit/metabo.js
@@ -0,0 +1,15 @@
+var metabo = document.getElementById("metabo");
+var metaboAd = document.createElement("div");
+metabo.appendChild(metaboAd);
+
+var metaboHref = document.createElement("a");
+metaboHref.href = "http://www.dag1688.com/products_brand_detail/&brandId=12.html";
+metaboHref.setAttribute("target","_blank");
+metaboAd.appendChild(metaboHref);
+
+var metaboImg = document.createElement("img");
+metaboImg.src = "http://www.dag1688.cc/img-store/metaboAd.jpg";
+metaboHref.appendChild(metaboImg);
+
+
+
---
+++ html_edit/weida.js
@@ -0,0 +1,15 @@
+var weida = document.getElementById("weida");
+var weidaAd = document.createElement("div");
+weida.appendChild(weidaAd);
+
+var weidaHref = document.createElement("a");
+weidaHref.href = "http://www.dag1688.com/products_brand_detail/&brandId=170.html";
+weidaHref.setAttribute("target","_blank");
+weidaAd.appendChild(weidaHref);
+
+var weidaImg = document.createElement("img");
+weidaImg.src = "http://www.dag1688.cc/img-store/weida-ad.jpg";
+weidaHref.appendChild(weidaImg);
+
+
+
